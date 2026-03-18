b7ff8c9f9c72718ce63d1ae94da8064a493cae0e
John Doe <johndoe@email.com>
2026-03-18 03:21:31 +0300

    logo

--- app/common/Header.tsx
+++ app/common/Header.tsx
@@ -30,6 +30,7 @@ import React from "react";
 import TopNavbar, { type TopNavItem } from "./TopNavbar";
 import ControlGroup from "./ControlGroup";
 import MobileTopActions from "./MobileTopActions";
+import Link from "next/link";
 
 type HeaderProps = {
   list: TopNavItem[];
@@ -53,7 +54,25 @@ const Header = ({ list }: HeaderProps) => {
         <ControlGroup />
 
         <div className="flex w-full items-center justify-between md:hidden">
-          <p className="text-foreground/80">logo</p>
+          {/* <p className="text-foreground/80">logo</p> */}
+          {/* Logo */}
+
+          <Link href="/" className=" flex items-center gap-3 px-2 group">
+            <img
+              src="/favicon.ico"
+              alt="PlayVibe Logo"
+              className="h-12 w-12 ml-0.5 object-contain"
+            />
+
+            <span
+              className="
+      whitespace-nowrap text-xl font-semibold text-foreground
+      opacity-0 transition-opacity duration-300 group-hover:opacity-100
+    "
+            >
+              Play<span className="text-primary">Vibe</span>
+            </span>
+          </Link>
           <MobileTopActions list={list} />
         </div>
       </div>
